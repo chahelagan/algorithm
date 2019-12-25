17f72ff03b7938cb05ff19014a2207349d1b3c1e
lagan.chahe <lagan.chahe@11bee.com>
2019-12-25 21:24:28 +0800

    20191225 push

---
+++ src/java/algo/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package algo;
+
+/**
+ * @author chahe
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        int length = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcada"));
+    }
+}
