f6374ad775da937129e85fa1f9a508d8f492352b
lagan.chahe <lagan.chahe@11bee.com>
2020-01-15 16:30:27 +0800

    2020-1-15 push todo

---
+++ src/java/algo/array/KthSmallestInLexicographicalOrder.java
@@ -0,0 +1,34 @@
+package algo.array;
+
+/**
+ * 给定整数 n 和 k，找到 1 到 n 中字典序第 k 小的数字。
+ *
+ * 注意：1 ≤ k ≤ n ≤ 109。
+ *
+ * 示例 :
+ *
+ * 输入:
+ * n: 13   k: 2
+ *
+ * 输出:
+ * 10
+ *
+ * 解释:
+ * 字典序的排列是 [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]，所以第二小的数字是 10。 todo
+ *
+ * @author chahe
+ * @since 2020-1-15
+ */
+public class KthSmallestInLexicographicalOrder {
+    public int findKthNumber(int n, int k) {
+        int result = 0;
+
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new KthSmallestInLexicographicalOrder().findKthNumber(13, 2));
+    }
+}
