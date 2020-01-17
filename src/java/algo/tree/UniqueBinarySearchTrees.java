ee54a082a51d7ed24b53f284ead2b749aefc1303
lagan.chahe <lagan.chahe@11bee.com>
2020-01-17 15:16:20 +0800

    2020-1-17 push

--- src/java/algo/tree/UniqueBinarySearchTrees.java
+++ src/java/algo/tree/UniqueBinarySearchTrees.java
@@ -20,10 +20,43 @@ package algo.tree;
  * @since 2020-1-16
  */
 public class UniqueBinarySearchTrees {
+    /**
+     * 先看定义:
+     *      g(x) = 节点数量为x时的 2叉树数量
+     *          g(0)=g(1)=1
+     *      f(a,b) = 以节点 a 为 root 节点量为b的 2叉树数量
+     *
+     *      g(x) = sum(f(i,x)) 0<=i<=x
+     *      f(i,x) = g(i-1) * g(x-1)
+     *      g(x) = sum(g(i-1) * g(x-1)) 0<=i<=x
+     * @param n
+     * @return
+     */
     public int numTrees(int n) {
-        int result = 0;
+        int[] g = new int[n + 1];
+        g[0] = 1;
+        g[1] = 1;
 
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                g[i] += g[j - 1] * g[i -j];
+            }
+        }
 
-        return result;
+        return g[n];
+    }
+
+    /**
+     *  卡塔兰数 C_n
+     * @param n
+     * @return
+     */
+    public int numTreesMath(int n) {
+        // Note: we should use long here instead of int, otherwise overflow
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
     }
 }
